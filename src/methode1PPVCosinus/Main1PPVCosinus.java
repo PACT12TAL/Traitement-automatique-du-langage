e8d348c11c7dd48f4896193794e0598852e2f4da
Nicolas Gianoli <nicolas.gianoli@telecom-paristech.fr>
2015-04-08 14:29:30 +0200

    mise en page

--- src/methode1PPVCosinus/Main1PPVCosinus.java
+++ src/methode1PPVCosinus/Main1PPVCosinus.java
@@ -15,25 +15,25 @@ public class Main1PPVCosinus {
 		new Methodes1PPVCosinus();
 	
 	
-	String parole="ajouter au panier";
+		String parole="je voudrais commencer";
 	
 	
-	int[] T = new int[Methodes1PPVCosinus.N]; 
-	// T est le tableau d'analyse du mot qui contiendra les nombres d'occurences des mots de la phrase a analyser
+		int[] T = new int[Methodes1PPVCosinus.N]; 
+			// T est le tableau d'analyse du mot qui contiendra les nombres d'occurences des mots de la phrase a analyser
 
 	
-	ArrayList<String> commande = Methodes1PPVCosinus.creerCommande(parole); 
-	/** commande est la arraylist de mots de la phrase a analyser*/
+		ArrayList<String> commande = Methodes1PPVCosinus.creerCommande(parole); 
+			/** commande est la arraylist de mots de la phrase a analyser*/
 	
-	T = Methodes1PPVCosinus.TableauAnalyse(commande);
+		T = Methodes1PPVCosinus.TableauAnalyse(commande);
 	
-	int indiceCommande = Methodes1PPVCosinus.tableauLePlusProche(T);
+		int indiceCommande = Methodes1PPVCosinus.tableauLePlusProche(T);
 	
-	int indiceAction = Methodes1PPVCosinus.correspondanceClasseAction(indiceCommande);
+		int indiceAction = Methodes1PPVCosinus.correspondanceClasseAction(indiceCommande);
 	
-	System.out.println(indiceCommande);
-	System.out.println(indiceAction); // indice a recuperer pour l'intégration
-	System.out.println(Methodes1PPVCosinus.toutesLesPhrases.get(indiceCommande));
+		System.out.println(indiceCommande);
+		System.out.println(indiceAction); // indice a recuperer pour l'intégration
+		System.out.println(Methodes1PPVCosinus.toutesLesPhrases.get(indiceCommande));
 	
 
 	}
